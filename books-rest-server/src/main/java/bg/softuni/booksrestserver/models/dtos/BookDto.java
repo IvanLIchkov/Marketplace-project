a16b76c63be5c4fb98c044a311c94832c214d0fd
Ivan Lichkov <Vanko.lichkov@abv.bg>
2023-10-12 12:39:36 +0300

    Some CRUD operations implementation.

---
+++ books-rest-server/src/main/java/bg/softuni/booksrestserver/models/dtos/BookDto.java
@@ -0,0 +1,54 @@
+package bg.softuni.booksrestserver.models.dtos;
+
+public class BookDto {
+
+    private Long id;
+
+    private String title;
+
+    private String isbn;
+
+    private AuthorDto author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public AuthorDto getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(AuthorDto author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "BookDto{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", author=" + author +
+                '}';
+    }
+}
---
+++ books-rest-server/src/main/java/bg/softuni/booksrestserver/models/entities/BookEntity.java
@@ -0,0 +1,50 @@
+package bg.softuni.booksrestserver.models.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class BookEntity extends BaseEntity{
+
+    private String title;
+
+    private String isbn;
+
+    @ManyToOne
+    private AuthorEntity author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public AuthorEntity getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(AuthorEntity author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "BookEntity{" +
+                "titile='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", author=" + (author != null ? author.getName() : null) +
+                '}';
+    }
+}
