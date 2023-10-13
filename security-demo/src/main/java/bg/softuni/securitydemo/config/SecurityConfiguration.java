3940dcfff3fbf7352ba4cf093677e294942c0c69
Ivan Lichkov <Vanko.lichkov@abv.bg>
2023-10-13 14:26:06 +0300

    User model.

---
+++ security-demo/src/main/java/bg/softuni/securitydemo/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package bg.softuni.securitydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests()
+                .anyRequest()
+                .permitAll();
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
