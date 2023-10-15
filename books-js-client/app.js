c1bdef8adccfde04af34a20cad8586d75b305895
Ivan Lichkov <Vanko.lichkov@abv.bg>
2023-10-15 13:49:19 +0300

    Implement delete books with rest api JS(fetch).

--- books-js-client/app.js
+++ books-js-client/app.js
@@ -5,23 +5,37 @@ let tBody = document.getElementById('books-container');
 
 loadBooksBtn.addEventListener("click", reloadBooks)
  async function reloadBooks() {
-  let requestOptions = {
-    method: 'GET'
-  }
 
-  fetch("http://localhost:8080/api/books", requestOptions)
+tBody.innerHTML='';
+
+  fetch("http://localhost:8080/api/books")
       .then(response => response.json())
       .then(json => json.forEach(book => {
           let tr = createElement('tr', tBody);
           let title = book.title;
           let author = book.author.name;
           let isbn = book.isbn;
+          let id = book.id;
 
           createElement('td',tr,title);
           createElement('td',tr,author);
           createElement('td',tr,isbn);
+          let deleteBtn = createElement('button', tr,'delete', null,null, {'book_id': id} );
+          deleteBtn.addEventListener('click', deleteBook);
+
       }));
 }
+
+function deleteBook(event) {
+
+    let bookId = event.target.attributes.getNamedItem('book_id').value;
+
+    fetch(`http://localhost:8080/api/books/${bookId}`,{
+        method: 'DELETE'})
+        .then(() => reloadBooks())
+        .catch(error => console.log('error' + error))
+}
+
 function createElement(type, parentNode, content, classes, id, attributes, useInnerHtml) {
     const htmlElement = document.createElement(type);
 
