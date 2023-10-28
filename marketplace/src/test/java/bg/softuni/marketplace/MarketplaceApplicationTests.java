d7e0bf466739031300bd0fd5da04c877c219eed3
Ivan Lichkov <Vanko.lichkov@abv.bg>
2023-10-28 11:42:54 +0300

    Marketplace project beginning.

---
+++ marketplace/src/test/java/bg/softuni/marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
